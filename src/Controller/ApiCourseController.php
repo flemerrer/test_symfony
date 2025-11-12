b9fde654d54eb0f1ff70507fba64a03bb8cef648
francois <francois.lemerrer@pandata.ninja>
2025-11-12 11:11:46 +0100

    Add sample api route to create an object dependant on another one.

---
+++ src/Controller/ApiCourseController.php
@@ -0,0 +1,52 @@
+<?php
+
+    namespace App\Controller;
+
+    use App\Entity\Course;
+    use App\Repository\CategoryRepository;
+    use Doctrine\ORM\EntityManagerInterface;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+    use Symfony\Component\HttpFoundation\Request;
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Attribute\Route;
+    use Symfony\Component\Serializer\SerializerInterface;
+    use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+    final class ApiCourseController extends AbstractController
+    {
+        #[Route('/api/courses', name: 'api_courses_create', methods: ['POST'])]
+        public function create(
+            Request                $request,
+            CategoryRepository     $categoryRepository,
+            EntityManagerInterface $em,
+            ValidatorInterface     $validator,
+            SerializerInterface    $serializer
+        ): JsonResponse
+        {
+            $data = $request->getContent();
+            $course = $serializer->deserialize($data, Course::class, 'json');
+
+            $dataArray = $request->toArray();
+            $category = $categoryRepository->find($dataArray['category']['id']);
+            if (!$category) {
+                return new JsonResponse(
+                    ['message' => 'Unknown category.'],
+                    Response::HTTP_BAD_REQUEST,
+                    []
+                );
+            }
+            $course->setCategory($category);
+
+            $errors = $validator->validate($course);
+            if (count($errors) > 0) {
+                $errorsJson = $serializer->serialize($errors, 'json');
+                return new JsonResponse($errorsJson, Response::HTTP_BAD_REQUEST, [], true);
+            }
+
+            $em->persist($course);
+            $em->flush();
+            // Can't use JsonResponse here because we need to set the context
+            return $this->json(null, Response::HTTP_CREATED, [], ['groups' => ['getCourse']]);
+        }
+    }
--- src/Entity/Course.php
+++ src/Entity/Course.php
@@ -1,171 +1,177 @@
 <?php
 
-namespace App\Entity;
+    namespace App\Entity;
 
-use App\Repository\CourseRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
+    use App\Repository\CourseRepository;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Doctrine\Common\Collections\Collection;
+    use Doctrine\DBAL\Types\Types;
+    use Doctrine\ORM\Mapping as ORM;
+    use Symfony\Component\Serializer\Annotation\Groups;
+    use Symfony\Component\Validator\Constraints as Assert;
 
-#[ORM\Entity(repositoryClass: CourseRepository::class)]
-class Course
-{
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    #[Groups(['getCategoriesFull'])]
-    private ?int $id = null;
-    #[ORM\Column(type: Types::STRING, length: 255, nullable: false)]
-    #[Groups(['getCategoriesFull'])]
-    private ?string $name = null;
-    #[ORM\Column(type: Types::STRING, length: 255, nullable: true)]
-    private ?string $content = null;
-    #[ORM\Column(type: Types::INTEGER, nullable: false)]
-    private ?int $duration = null;
-    #[ORM\Column(type: Types::BOOLEAN, nullable: false)]
-    private ?bool $published = null;
-    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: false)]
-    private ?\DateTimeImmutable $dateCreated = null;
-    #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
-    private ?\DateTimeImmutable $dateModified = null;
-
-    #[ORM\ManyToOne(inversedBy: 'courses')]
-    private ?Category $category = null;
-
-    /**
-     * @var Collection<int, Trainer>
-     */
-    #[ORM\ManyToMany(targetEntity: Trainer::class, inversedBy: 'courses')]
-    private Collection $trainers;
-
-    public function __construct()
+    #[ORM\Entity(repositoryClass: CourseRepository::class)]
+    class Course
     {
-        $this->trainers = new ArrayCollection();
-        $this->setDateCreated(new \DateTimeImmutable());
-    }
+        #[ORM\Id]
+        #[ORM\GeneratedValue]
+        #[ORM\Column]
+        #[Groups(['getCategoriesFull, getCourse'])]
+        private ?int $id = null;
+        #[ORM\Column(type: Types::STRING, length: 255, nullable: false)]
+        #[Groups(['getCategoriesFull, getCourse'])]
+        private ?string $name = null;
+        #[ORM\Column(type: Types::STRING, length: 255, nullable: true)]
+        #[Groups(['getCourse'])]
+        private ?string $content = null;
+        #[Assert\NotBlank()]
+        #[Assert\Range(min: 1, max: 50, notInRangeMessage: 'Duration must be between 1 and 50!')]
+        #[ORM\Column(type: Types::INTEGER, nullable: false)]
+        #[Groups(['getCourse'])]
+        private ?int $duration = null;
+        #[ORM\Column(type: Types::BOOLEAN, nullable: false)]
+        #[Groups(['getCourse'])]
+        private ?bool $published = null;
+        #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: false)]
+        private ?\DateTimeImmutable $dateCreated = null;
+        #[ORM\Column(type: Types::DATETIME_IMMUTABLE, nullable: true)]
+        private ?\DateTimeImmutable $dateModified = null;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+        #[ORM\ManyToOne(inversedBy: 'courses')]
+        private ?Category $category = null;
 
-    public function getName(): string
-    {
-        return $this->name;
-    }
+        /**
+         * @var Collection<int, Trainer>
+         */
+        #[ORM\ManyToMany(targetEntity: Trainer::class, inversedBy: 'courses')]
+        private Collection $trainers;
 
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
-
-    public function getContent(): string
-    {
-        return $this->content;
-    }
-
-    public function setContent(string $content): void
-    {
-        $this->content = $content;
-    }
-
-    public function getDuration(): string
-    {
-        return $this->duration;
-    }
-
-    public function setDuration(string $duration): void
-    {
-        $this->duration = $duration;
-    }
-
-    public function isPublished(): bool
-    {
-        return $this->published;
-    }
-
-    public function setPublished(bool $published): void
-    {
-        $this->published = $published;
-    }
-
-    public function getDateCreated(): string
-    {
-        if ($this->dateCreated != null) {
-            return $this->dateCreated->format("d/m/y");
-        }
-        return "";
-    }
-
-    public function setDateCreated(\DateTimeImmutable $dateCreated): void
-    {
-        $this->dateCreated = $dateCreated;
-    }
-
-    public function getDateModified(): string
-    {
-        if ($this->dateModified != null) {
-            return $this->dateModified->format("d/m/y");
-        }
-        return "";
-    }
-
-    public function setDateModified(\DateTimeImmutable $dateModified): void
-    {
-        $this->dateModified = $dateModified;
-    }
-
-    public function getPublished(): ?bool
-    {
-        return $this->published;
-    }
-
-    public function __toString(): string
-    {
-        return "name: " . $this->getName() . "(". $this->getId() . ")\n"
-            . "duration: " . $this->getDuration() . "\n"
-            . "published: " . $this->getPublished() . "\n"
-            . "dateCreated: " . $this->getDateCreated() . "\n"
-            . "dateModified: " . $this->getDateModified();
-    }
-
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
-
-    public function setCategory(?Category $category): static
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Trainer>
-     */
-    public function getTrainers(): Collection
-    {
-        return $this->trainers;
-    }
-
-    public function addTrainer(Trainer $trainer): static
-    {
-        if (!$this->trainers->contains($trainer)) {
-            $this->trainers->add($trainer);
+        public function __construct()
+        {
+            $this->trainers = new ArrayCollection();
+            $this->setDateCreated(new \DateTimeImmutable());
         }
 
-        return $this;
+        public function getId(): ?int
+        {
+            return $this->id;
+        }
+
+        public function getName(): string
+        {
+            return $this->name;
+        }
+
+        public function setName(string $name): void
+        {
+            $this->name = $name;
+        }
+
+        public function getContent(): string
+        {
+            return $this->content;
+        }
+
+        public function setContent(string $content): void
+        {
+            $this->content = $content;
+        }
+
+        public function getDuration(): string
+        {
+            return $this->duration;
+        }
+
+        public function setDuration(string $duration): void
+        {
+            $this->duration = $duration;
+        }
+
+        public function isPublished(): bool
+        {
+            return $this->published;
+        }
+
+        public function setPublished(bool $published): void
+        {
+            $this->published = $published;
+        }
+
+        public function getDateCreated(): string
+        {
+            if ($this->dateCreated != null) {
+                return $this->dateCreated->format("d/m/y");
+            }
+            return "";
+        }
+
+        public function setDateCreated(\DateTimeImmutable $dateCreated): void
+        {
+            $this->dateCreated = $dateCreated;
+        }
+
+        public function getDateModified(): string
+        {
+            if ($this->dateModified != null) {
+                return $this->dateModified->format("d/m/y");
+            }
+            return "";
+        }
+
+        public function setDateModified(\DateTimeImmutable $dateModified): void
+        {
+            $this->dateModified = $dateModified;
+        }
+
+        public function getPublished(): ?bool
+        {
+            return $this->published;
+        }
+
+        public function __toString(): string
+        {
+            return "name: " . $this->getName() . "(" . $this->getId() . ")\n"
+                . "duration: " . $this->getDuration() . "\n"
+                . "published: " . $this->getPublished() . "\n"
+                . "dateCreated: " . $this->getDateCreated() . "\n"
+                . "dateModified: " . $this->getDateModified();
+        }
+
+        public function getCategory(): ?Category
+        {
+            return $this->category;
+        }
+
+        public function setCategory(?Category $category): static
+        {
+            $this->category = $category;
+
+            return $this;
+        }
+
+        /**
+         * @return Collection<int, Trainer>
+         */
+        public function getTrainers(): Collection
+        {
+            return $this->trainers;
+        }
+
+        public function addTrainer(Trainer $trainer): static
+        {
+            if (!$this->trainers->contains($trainer)) {
+                $this->trainers->add($trainer);
+            }
+
+            return $this;
+        }
+
+        public function removeTrainer(Trainer $trainer): static
+        {
+            $this->trainers->removeElement($trainer);
+
+            return $this;
+        }
+
+
     }
-
-    public function removeTrainer(Trainer $trainer): static
-    {
-        $this->trainers->removeElement($trainer);
-
-        return $this;
-    }
-
-
-}
